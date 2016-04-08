b7bae25d44d19c302b2fe7ae6c3661b5c097ffa1
thsconline <thsconline@gmail.com>
2016-04-08 18:26:07 +1000

    Update

--- scripts/thsc-system-script.js
+++ scripts/thsc-system-script.js
@@ -32,7 +32,7 @@ document.write("<img src=\"https:\/\/thsconline.github.io\/s\/images\/icon_pdf2.
 document.write("<a href=\"https:\/\/drive.google.com\/uc?export=download&id="+idx+"\" target=\"_blank\">Download File<\/a>&nbsp;&nbsp;<wbr>");
 document.write("<span id=\"reduciblex\"><img src=\"https:\/\/thsconline.github.io\/s\/images\/icon_back.png\">&nbsp;&nbsp;");
 document.write("<a href=\"index.html\">Up One Folder<\/a>&nbsp;&nbsp;<wbr>");
-document.write("<img id=\"reduciblex\" src=\"https:\/\/thsconline.github.io\/s\/images\/icon_home.png\">&nbsp;&nbsp;");
+document.write("<img id=\"reducible\" src=\"https:\/\/thsconline.github.io\/s\/images\/icon_home.png\">&nbsp;&nbsp;");
 document.write("<a href=\"https:\/\/thsconline.github.io\/s\/\">Home<\/a>&nbsp;&nbsp;</span></span></span><br>");
 document.write("<style>.drive-viewer-popout-button{display:none;}</style><iframe style=\"width:100%; height:95%\" id=\"mediaplayer\" src=\"https:\/\/drive.google.com\/file\/d\/"+idx+"\/preview\"><noscript>&nbsp;Enable Javascript to Load File<\/noscript><\/iframe>");
 document.write("</div></body></html>"); 
