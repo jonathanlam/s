e690ba985b0b8228b3fa3c007cf737ed845f7bf4
thsconline <thsconline@gmail.com>
2016-04-08 12:45:29 +1000

    Update

--- scripts/thsc-system-script.js
+++ scripts/thsc-system-script.js
@@ -11,7 +11,7 @@ setTimeout(function(){window.location=link}, 3000);
 function lf(titlex,idx, tags){
     var tix=document.title;
 document.write("<html><head>");
-document.write("<title>"+tix+" - Now Playing: "+titlex.replace("<i>", "").replace("</i>", "") +"<\/title>");
+document.write("<title>"+tix+" - "+titlex.replace("<i>", "").replace("</i>", "") +"<\/title>");
 document.write("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\">");
 document.write("<meta http-equiv=\"content-type\" content=\"text\/html; charset=utf-8\">");
 document.write("<link href=\"https:\/\/thsconline.github.io\/s\/styles\/style.css\" rel=\"stylesheet\" type=\"text\/css\">");
@@ -22,7 +22,7 @@ document.write("<body>");
 document.write("<div id=\"sites-chrome-everything\">");
 document.write("<span style=\"background-color:rgb(248, 248, 248);width:100%;display:inline-block;\" class=\"goog-tree-item-label\">");
 document.write("<img src=\"https:\/\/thsconline.github.io\/s\/images\/icon_back.png\">&nbsp;&nbsp;");
-document.write("<a href=\""+tags+"\">Exit File Viewer<\/a>&nbsp;&nbsp;<wbr><small><b>File: </b>"+titlex+"</small>");
+document.write("<a href=\""+tags+"\">Exit File Viewer<\/a>&nbsp;&nbsp;<wbr><small><b>"+tix.split(" - ")+" </b>"+titlex+"</small>");
 document.write("<span style=\"float:right\">");
 document.write("<img src=\"https:\/\/thsconline.github.io\/s\/images\/icon_pdf2.png\">&nbsp;&nbsp;");
 document.write("<a href=\""+idx+"\" target=\"_blank\">Open File<\/a>&nbsp;&nbsp;<wbr>");
