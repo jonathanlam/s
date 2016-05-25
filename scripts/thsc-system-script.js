e51baaea1a44bf0e24be6b568996ee0092b77a2e
thsconline <thsconline@gmail.com>
2016-05-25 13:43:14 +1000

    Code Fix 2

--- scripts/thsc-system-script.js
+++ scripts/thsc-system-script.js
@@ -11,7 +11,7 @@ var tix=document.title;
 var ua = window.navigator.userAgent; var msie = ua.indexOf("MSIE ");
 if (msie > 0){var version=parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)));
 if(version>9){} else {input.onclick=function(){return true}; document.getElementById(idx).click();
-input.insertAdjacentHTML(afterend, "<span class=\"new\">File Loaded<\/span>");return true;
+input.insertAdjacentHTML("afterend", "<span class=\"new\">File Loaded<\/span>");return true;
 
 
 }}
@@ -45,7 +45,7 @@ var tix=document.title;
 var ua = window.navigator.userAgent; var msie = ua.indexOf("MSIE ");
 if (msie > 0){var version=parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)));
 if(version>9){} else {input.onclick=function(){return true}; document.getElementById(idx).click();
-input.insertAdjacentHTML(afterend, "<span class=\"new\">File Loaded<\/span>");return true;
+input.insertAdjacentHTML("afterend", "<span class=\"new\">File Loaded<\/span>");return true;
 }}
 document.write("<html><head>");
 document.write("<title>"+tix+" - "+titlex.replace("<i>", "").replace("</i>", "") +"<\/title>");
